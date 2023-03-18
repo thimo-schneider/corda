9d80840038ccfc797cfb46d61a3bfd8214aa5b41
Thimo <schneider.thimo@protonmail.com>
2023-03-18 14:31:26 +0100

    Add partial deploy functionality

--- src/kubernetes/deploy.ts
+++ src/kubernetes/deploy.ts
@@ -1,15 +1,17 @@
 import * as k8s from '@kubernetes/client-node';
 import { loadAndModifyKubeSpec } from '../helpers/yaml';
 import apply from "./apply";
+import constants from "../constants"
+const { POD_TEMPLATE_FILE_PATH } = constants
 
 export async function deployPod(namespace: string, name: string, image: string): Promise<k8s.KubernetesObject> {
   const modifiers = [{ path: "metadata.namespace", value: namespace }, {
-    path: "metadata.namespace", value: name
+    path: "metadata.name", value: name
   }, {
-    path: "spec.containers.image", value: image
+    path: "spec.containers[0].image", value: image
   }]
-  const spec = await loadAndModifyKubeSpec(modifiers, "../templates/kubernetes/pod.yml")
-  const result = await apply(spec)
-  console.log(result)
+  const spec = await loadAndModifyKubeSpec(modifiers, POD_TEMPLATE_FILE_PATH)
+  const result: k8s.KubernetesObject = await apply(spec)
   return result
 }
+

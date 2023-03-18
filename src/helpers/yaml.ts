4ff36cdfa50ce891d43149acc04ee7789e84a278
Thimo <schneider.thimo@protonmail.com>
2023-03-18 14:27:58 +0100

    Add helper functions for yaml modification

--- src/helpers/yaml.ts
+++ src/helpers/yaml.ts
@@ -1,7 +1,8 @@
 import yaml from "js-yaml"
 import fs from "fs/promises"
-// import f from "lodash/fp"
+import f from "lodash/fp"
 import * as k8s from '@kubernetes/client-node';
+import path from "path"
 
 export type YamlModifier = { path: string, value: string }
 
@@ -11,17 +12,23 @@ export async function loadKubeSpec(filePath: string): Promise<k8s.KubernetesObje
 }
 
 export function modifyKubeSpec(modifiers: YamlModifier[], spec: k8s.KubernetesObject): k8s.KubernetesObject {
-  // return f.reduce((acc: k8s.KubernetesObject, val: YamlModifier) => {
-  //   const { path, value } = val
-  //   return f.set(path, value, acc)
-  // }, spec, modifiers)
-  return spec
+  return f.reduce((acc: k8s.KubernetesObject, val: YamlModifier) => {
+    const { path, value } = val
+    const res = f.set(path, value, acc)
+    return res
+  }, spec, modifiers)
 }
 
-export async function loadAndModifyKubeSpec(modifiers: YamlModifier[], filePath: string): Promise<k8s.KubernetesObject> {
-  return loadKubeSpec(filePath).then(spec => modifyKubeSpec(modifiers, spec))
+export async function loadAndModifyKubeSpec(modifiers: YamlModifier[], absFilePath: string): Promise<k8s.KubernetesObject> {
+  // const absFilePath = path.join(__dirname, filePath)
+  return loadKubeSpec(absFilePath).then(spec => modifyKubeSpec(modifiers, spec))
 }
 
-// export async function dumpYaml(filePath: string): Promise<void> {
-
-// }
+export async function createPodSpec(namespace: string, name: string, containerImage: string, templateFilePath: string): Promise<k8s.KubernetesObject> {
+  const modifiers = [{ path: "metadata.namespace", value: namespace }, {
+    path: "metadata.name", value: name
+  }, {
+    path: "spec.containers[0].image", value: containerImage
+  }]
+  return loadAndModifyKubeSpec(modifiers, templateFilePath)
+}

1c481bcc5da27efb9e96a9f0de6892054f8e2123
Thimo <schneider.thimo@protonmail.com>
2023-04-06 10:42:59 +0200

    Remove unused scripts

--- conv.js
+++
@@ -1,9 +0,0 @@
-const convert = require('tsconfig-to-swcconfig').convert
-const fs = require("fs")
-
-const conf = convert('tsconfig.json', process.cwd(), {
-  // more swc config to override...
-  minify: false,
-})
-fs.writeFileSync("./.swcrc", JSON.stringify(conf))
-console.log(conf)
--- conv.ts
+++
@@ -1,6 +0,0 @@
-import { convert } from 'tsconfig-to-swcconfig'
-
-convert('tsconfig-filename.json', process.cwd(), {
-  // more swc config to override...
-  minify: false,
-})

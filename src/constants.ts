67926eb3846e130efc02307db9de916a2f542538
Thimo <schneider.thimo@protonmail.com>
2023-04-12 13:16:35 +0200

    Add PVC

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,6 @@ export default {
   REPRODUCIBLE_POD_TEMPLATE_FILE_PATH: path.resolve("./dist", "./templates/kubernetes/reproducible_pod.yml"),
   WORKFLOW_DESCRIPTOR_CONFIG_MAP_TEMPLATE_FILE_PATH: path.resolve("./dist", "./templates/kubernetes/workflow_descriptor_config_map.yml"),
   IRODS_PASSWORD_SECRET_TEMPLATE_FILE_PATH: path.resolve("./dist", "./templates/kubernetes/irods_password_secret.yml"),
-  GIT_PASSWORD_SECRET_TEMPLATE_FILE_PATH: path.resolve("./dist", "./templates/kubernetes/git_token_secret.yml")
+  GIT_PASSWORD_SECRET_TEMPLATE_FILE_PATH: path.resolve("./dist", "./templates/kubernetes/git_token_secret.yml"),
+  PVC_TEMPLATE_FILE_PATH: path.resolve("./dist", "./templates/kubernetes/pvc.yml")
 }
--- src/kubernetes/runReproducibly.ts
+++ src/kubernetes/runReproducibly.ts
@@ -3,17 +3,25 @@ import fs from "fs/promises"
 import { loadAndModifyKubeSpec } from '../helpers/yaml';
 import apply from "./apply";
 import constants from "../constants"
-const { REPRODUCIBLE_POD_TEMPLATE_FILE_PATH,WORKFLOW_DESCRIPTOR_CONFIG_MAP_TEMPLATE_FILE_PATH,IRODS_PASSWORD_SECRET_TEMPLATE_FILE_PATH,GIT_PASSWORD_SECRET_TEMPLATE_FILE_PATH  } = constants
+const { REPRODUCIBLE_POD_TEMPLATE_FILE_PATH,WORKFLOW_DESCRIPTOR_CONFIG_MAP_TEMPLATE_FILE_PATH,IRODS_PASSWORD_SECRET_TEMPLATE_FILE_PATH,GIT_PASSWORD_SECRET_TEMPLATE_FILE_PATH,PVC_TEMPLATE_FILE_PATH  } = constants
 
 export async function runReproducibly(namespace: string, name: string, workflowDescriptorPath: string, irodsPassword: string, gitPassword: string, runMode: string = "REPRODUCIBLE"): Promise<k8s.KubernetesObject> {
   const configMapName = name + "-config-map"
   const irodsPasswordSecretName = "irods-password"
   const gitPasswordSecretName = name + "-git-password"
+  const pvcName = name + "-pvc"
   const workflowDescriptorString = await fs.readFile(workflowDescriptorPath, "utf8")
   const workflowDescriptor = JSON.parse(workflowDescriptorString)
-  const {image} = workflowDescriptor  
+  const {image} = workflowDescriptor
   const baseModifiers = [
     { path: "metadata.namespace", value: namespace },
+  ]
+
+  const maybePVCModifier = runMode === "REPRODUCIBLE" ? [] : baseModifiers.concat([{path: "metadata.name", value: pvcName}])
+  const maybePVCPodModifier = runMode === "REPRODUCIBLE" ? [] : [
+    {path: "spec.containers[0].volumeMounts[1].name", value: pvcName},
+    {path:"spec.volumes[1].name", value: pvcName},
+    {path:"spec.volumes[1].persistentVolumeClaim.claimName", value: pvcName}
   ]
   const podModifiers = baseModifiers.concat([
     {path: "metadata.name", value: name},
@@ -21,7 +29,7 @@ export async function runReproducibly(namespace: string, name: string, workflowD
     {path: "spec.volumes[0].configMap.name", value: configMapName},
     {path: "spec.containers[0].env[1].valueFrom.secretKeyRef.name", value: gitPasswordSecretName},
     {path: "spec.containers[0].env[2].value", value: runMode},
-  ])
+  ]).concat(maybePVCPodModifier)
   const workflowDescriptorModifiers = baseModifiers.concat([
     {path: "metadata.name", value: configMapName},
     {path: ["data", "workflow_descriptor.json"], value: workflowDescriptorString}
@@ -36,11 +44,13 @@ export async function runReproducibly(namespace: string, name: string, workflowD
   ])
 
 
+  const pvcSpec = await loadAndModifyKubeSpec(maybePVCModifier, PVC_TEMPLATE_FILE_PATH)
   const podSpec = await loadAndModifyKubeSpec(podModifiers, REPRODUCIBLE_POD_TEMPLATE_FILE_PATH)
   const workflowDescriptorConfigMapSpec = await loadAndModifyKubeSpec(workflowDescriptorModifiers, WORKFLOW_DESCRIPTOR_CONFIG_MAP_TEMPLATE_FILE_PATH)
   const irodsPasswordSecretSpec = await loadAndModifyKubeSpec(irodsAuthModifiers, IRODS_PASSWORD_SECRET_TEMPLATE_FILE_PATH)
   const gitPasswordSecretSpec = await loadAndModifyKubeSpec(gitPasswordModifiers, GIT_PASSWORD_SECRET_TEMPLATE_FILE_PATH)
 
+  const pvcResult = await apply(pvcSpec)
   const gitPasswordResult = await apply(gitPasswordSecretSpec)
   const irodsAuthResult = await apply(irodsPasswordSecretSpec)
   const workflowDescriptorResult = await apply(workflowDescriptorConfigMapSpec)

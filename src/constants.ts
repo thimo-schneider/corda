2ba8fd8fea0eb5d1033886bceb288d2e0a9c624e
Thimo <schneider.thimo@protonmail.com>
2023-04-15 12:05:01 +0200

    Fix path resolution for template files

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 import path from "path"
 export default {
-  POD_TEMPLATE_FILE_PATH: path.resolve("./dist", "./templates/kubernetes/pod.yml"),
-  REPRODUCIBLE_POD_TEMPLATE_FILE_PATH: path.resolve("./dist", "./templates/kubernetes/reproducible_pod.yml"),
-  WORKFLOW_DESCRIPTOR_CONFIG_MAP_TEMPLATE_FILE_PATH: path.resolve("./dist", "./templates/kubernetes/workflow_descriptor_config_map.yml"),
-  IRODS_PASSWORD_SECRET_TEMPLATE_FILE_PATH: path.resolve("./dist", "./templates/kubernetes/irods_password_secret.yml"),
-  GIT_PASSWORD_SECRET_TEMPLATE_FILE_PATH: path.resolve("./dist", "./templates/kubernetes/git_token_secret.yml"),
-  PVC_TEMPLATE_FILE_PATH: path.resolve("./dist", "./templates/kubernetes/pvc.yml")
+  POD_TEMPLATE_FILE_PATH: path.resolve(__dirname,  "templates/kubernetes/pod.yml"),
+  REPRODUCIBLE_POD_TEMPLATE_FILE_PATH: path.resolve(__dirname, "templates/kubernetes/reproducible_pod.yml"),
+  WORKFLOW_DESCRIPTOR_CONFIG_MAP_TEMPLATE_FILE_PATH: path.resolve(__dirname, "templates/kubernetes/workflow_descriptor_config_map.yml"),
+  IRODS_PASSWORD_SECRET_TEMPLATE_FILE_PATH: path.resolve(__dirname, "templates/kubernetes/irods_password_secret.yml"),
+  GIT_PASSWORD_SECRET_TEMPLATE_FILE_PATH: path.resolve(__dirname, "templates/kubernetes/git_token_secret.yml"),
+  PVC_TEMPLATE_FILE_PATH: path.resolve(__dirname, "templates/kubernetes/pvc.yml")
 }
